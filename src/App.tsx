eb0e2dd68315ffc988c15874e66ae5e9d65454b2
gcpearse <glen.c.pearse@gmail.com>
2023-12-21 18:13:08 +0000

    updates components

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import './App.css'
+import Footer from './components/Footer'
+import Header from './components/Header'
 
 function App() {
   return (
-    <h1>Gyroselector</h1>
+    <>
+      <Header />
+      <Footer />
+    </>
   )
 }
 
